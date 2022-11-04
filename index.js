b45a0dc844e6f538fee8f6b5604c246a78bd04ab
chi_duong <anhchictw311@gmail.com>
2022-11-04 09:34:11 +0700

    fix ssl

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 
 import express from 'express'
 const app = express()
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const port = 3001
 
 
 // parse application/x-www-form-urlencoded
 // parse application/json
-
+app.use(express.static( __dirname + '/public'));
 app.get('/',(req,res)=>{
     res.send(`success`)
 })
